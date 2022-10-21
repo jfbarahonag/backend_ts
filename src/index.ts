38a29a1eeb7192d34906586f4a83e80e8ec31113
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2022-10-20 21:37:18 -0500

    First commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json()) //req.body to json
+
+const PORT = 3000
+
+app.get('/ping', (_req, res) => { //_req to avoid warnings
+    console.log(`ping - ${new Date().toLocaleDateString()}`);
+    res.send('pong')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    
+})
