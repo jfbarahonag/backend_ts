532e887db7c76680b6953f0b9e63123d55239b35
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2022-10-20 21:55:35 -0500

    routes for diary entries were added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import diaryRouter from './routes/diaries'
+
 const app = express()
 app.use(express.json()) //req.body to json
 
@@ -9,6 +11,8 @@ app.get('/ping', (_req, res) => { //_req to avoid warnings
     console.log(`ping - ${new Date().toLocaleDateString()}`);
     res.send('pong!!!')
 })
+
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ src/routes/diaries.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const diaryRouter = express.Router()
+
+diaryRouter.get('/', (_req, res) => {
+    res.send('Fetching all entry diaries')
+})
+
+diaryRouter.post('/', (_req, res) => {
+    res.send('Saving a diary')
+})
+
+export default diaryRouter
